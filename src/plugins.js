20a7f3544ceb1d6afba415214718a5f829c5484a
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-04-09 02:06:22 +0200

    testing `buffer`

--- src/plugins.js
+++ src/plugins.js
@@ -51,7 +51,7 @@ class Plugins {
       // Must be awaited
 const attachment = await discordTranscripts.createTranscript(channel, {
   limit: -1, // Max amount of messages to fetch. `-1` recursively fetches.
-  returnType: 'attachment', // Valid options: 'buffer' | 'string' | 'attachment' Default: 'attachment' OR use the enum ExportReturnType
+  returnType: 'buffer', // Valid options: 'buffer' | 'string' | 'attachment' Default: 'attachment' OR use the enum ExportReturnType
   filename: 'transcript.html', // Only valid with returnType is 'attachment'. Name of attachment.
   saveImages: true, // Download all images and include the image data in the HTML (allows viewing the image even after it has been deleted) (! WILL INCREASE FILE SIZE !)
   footerText: "Exported {number} message{s}", // Change text at footer, don't forget to put {number} to show how much messages got exported, and {s} for plural

3862879ac6c055441f46dd38aace805b30478ab8
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-07-09 02:23:11 +0300

    `$randomTopic`

--- src/plugins.js
+++ src/plugins.js
@@ -24,6 +24,14 @@ class Plugins {
       }
     });
 
+
+     bot.functionManager.createFunction({
+    name: "$randomTopic", 
+    params: [], 
+    type: "aoi.js", 
+    code: "$randomText[Did you go out today?;What did you eat today?;What are you currently worried about?;What game have you spent the most hours playing?;What game have you spent the most hours playing?;What movie can you watch over and over without ever getting tired of?;What are you interested in that most people aren't?;How do you judge a person?;What do you think you are much better at than you actually are?;Do you enjoy spicy food?]" 
+});
+    
     bot.functionManager.createFunction({
     name: "$mentionAuthor", 
     params: [], 
